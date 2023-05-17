ed923ccb3e6fc288edf9bf9d6fdd9056bb0c9cf4
h4seeb-cmd <mirzahbeg123@gmail.com>
2023-05-17 08:05:35 -0700

    attempting api get

--- soundTest.py
+++ soundTest.py
@@ -6,7 +6,7 @@ pygame.init()
 music = ["soundTestFiles/bravo.mp3", "soundTestFiles/COMET_demo.mp3", "soundTestFiles/SPACE_JAMZdemo3.mp3"]
 
 
-pygame.mixer.music.load(music[int(input("what track do you want to listen to? (1-3)")) - 1])
+pygame.mixer.music.load(music[int(input("wha3t track do you want to listen to? (1-3)")) - 1])
 
 pygame.mixer.music.play()
 
---
+++ spotifyAPI.py
@@ -0,0 +1,5 @@
+import requests
+
+# albumAPI=requests.get("https://api.spotify.com/v1/albums/4aawyAB9vmqN3uQ7FjRGTy")
+# print(albumAPI.status_code)
+
