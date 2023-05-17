76caaba98cdb7eda3b26c41b339729bb774daa59
h4seeb-cmd <mirzahbeg123@gmail.com>
2023-05-17 07:46:40 -0700

    adding randomization

--- soundTest.py
+++ soundTest.py
@@ -1,14 +1,16 @@
 import pygame
-
+from random import randint
 
 pygame.init()
 
-pygame.mixer.music.load("soundTestFiles/bravo.mp3")
+music = ["soundTestFiles/bravo.mp3", "soundTestFiles/COMET_demo.mp3", "soundTestFiles/SPACE_JAMZdemo3.mp3"]
+
+
+pygame.mixer.music.load(music[randint(0,2)])
 
 pygame.mixer.music.play()
 
 pygame.time.wait(10000)
 
 pygame.quit()
-
 
